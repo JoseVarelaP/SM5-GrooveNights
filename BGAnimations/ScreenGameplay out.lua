8ed3e8b7061a9aac5ae0b12c5832cb9b554ecb9e
JoseVarelaP <josevarela@tutanota.com>
2020-01-08 13:08:31 -0600

    Fix Out Transition audio

--- BGAnimations/ScreenGameplay out.lua
+++ BGAnimations/ScreenGameplay out.lua
@@ -1,5 +1,25 @@
-return Def.Quad{
-    OnCommand=function(s) s:stretchto(SCREEN_WIDTH,SCREEN_HEIGHT,0,0)
-        :diffuse(Color.Black):diffusealpha(0):sleep(2):linear(0.4):diffusealpha(1)
-    end
+return Def.ActorFrame{
+    OnCommand=function(s)
+        s:hibernate(1)
+    end,
+    Def.Quad{
+        OnCommand=function(s) s:stretchto(SCREEN_WIDTH,SCREEN_HEIGHT/2,0,SCREEN_HEIGHT):diffuse(Color.
+        Black):addy( SCREEN_HEIGHT/2 ):decelerate(0.2):addy( -SCREEN_HEIGHT/2 )
+        end
+    },
+    Def.Quad{
+        OnCommand=function(s) s:stretchto(SCREEN_WIDTH,SCREEN_HEIGHT/2,0,0):diffuse(Color.Black):addy( -SCREEN_HEIGHT/2 )
+            :decelerate(0.2):addy( SCREEN_HEIGHT/2 )
+        end
+    },
+
+    Def.Sprite{
+        Texture=THEME:GetPathG("","TransitionArrow"),
+        OnCommand=function(s)
+            s:xy(SCREEN_CENTER_X,SCREEN_BOTTOM+100):queuecommand("SFX"):decelerate(0.2):y( SCREEN_CENTER_Y )
+        end,
+        SFXCommand=function(s)
+            SOUND:PlayOnce( THEME:GetPathS("gnScreenTransition whoosh", "in") )
+        end,
+    },
 }
