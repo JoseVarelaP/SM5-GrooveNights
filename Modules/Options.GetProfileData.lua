daed227c29cde688bc5991d94d8feea2f2bc08c4
Jose_Varela <jo.lu6853@gmail.com>
2022-01-01 18:34:58 -0600

    Add support for image loading from memory/profile

--- Modules/Options.GetProfileData.lua
+++ Modules/Options.GetProfileData.lua
@@ -6,20 +6,37 @@ return function(PData)
 		[PData] = {}
 	};
 	Info.PData = { Name="", Image="" };
+
+	local function FindPictureFromDirectory( Directory )
+		local Dir = FILEMAN:GetDirListing(Directory)
+		for _,v in ipairs(Dir) do
+			if string.match(v, "(%w+)") == profile:GetDisplayName() then
+				Info[PData].Image = Directory..v
+			end
+		end
+	end
 	
 	Info[PData].Image = THEME:GetPathG("","NoAvatar/".. (string.find(PData, "P1") and "P1" or "P2") .."noavatar" )
 	Info[PData].Name = "No Card"
 	if profile and profile:GetDisplayName() ~= "" then
 		Info[PData].Name = profile:GetDisplayName()
-		local Dir = FILEMAN:GetDirListing("/Appearance/Avatars/")
+
+		-- Is it available on the profile itself?
+		local Dir = FILEMAN:GetDirListing(PROFILEMAN:GetProfileDir(Pslot-1))
 		for _,v in ipairs(Dir) do
-			if string.match(v, "(%w+)") == profile:GetDisplayName() then Info[PData].Image = "/Appearance/Avatars/"..v end
+			-- is there a avatar image?
+			if string.find(v, "avatar") then
+				Info[PData].Image = PROFILEMAN:GetProfileDir(Pslot-1)..v
+			end
+			-- or the name matches the profile name?
+			if string.match(v, "(%w+)") == profile:GetDisplayName() then
+				Info[PData].Image = PROFILEMAN:GetProfileDir(Pslot-1)..v
+			end
 		end
+		-- Is it available on OutFox's avatars directory?
+		FindPictureFromDirectory("/Appearance/Avatars/")
 		-- Is the image still generic? Let's try chceking the built in avatars.
-		Dir = FILEMAN:GetDirListing( THEME:GetCurrentThemeDirectory().."/Graphics/NoAvatar/" )
-		for _,v in ipairs(Dir) do
-			if string.match(v, "(%w+)Avatar") == profile:GetDisplayName() then Info[PData].Image = THEME:GetPathG("","NoAvatar/"..v) end
-		end
+		FindPictureFromDirectory(THEME:GetCurrentThemeDirectory().."/Graphics/NoAvatar/")
 	end
 	if GAMESTATE:Env()["Konami"] then
 		Info[PData].Image = THEME:GetPathG("","NoAvatar/KonamiAvatar")
